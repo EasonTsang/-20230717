c5fdc9b17d43eda0cb7e6bbf4778f44621c110e9
easonT <2290800118@qq.com>
2023-07-20 12:32:05 +0800

    problem 1 fix bug

--- problem1.py
+++ problem1.py
@@ -19,6 +19,55 @@ df.insert(0, '订单完成率',df['总供货量']/df['总订货量'])
 df.drop('总订货量',axis=1,inplace=True)
 print(df)
 
+# 中间型指标 -> 极大型指标
+def dataDirection_2(datas, x_best):
+    temp_datas = datas - x_best
+    M = np.max(abs(temp_datas))
+    answer_datas = 1 - abs(datas - x_best) / M  # 套公式
+    return answer_datas
+
+df['订单完成率']=dataDirection_2(df['订单完成率'],1)
+print(df)
+
+# # 正向化矩阵标准化
+# def temp2(datas):
+#     K = np.power(np.sum(pow(datas, 2), axis=1), 0.5)
+#     for i in range(0, K.size):
+#         for j in range(0, datas[i].size):
+#             datas[i, j] = datas[i, j] / K[i]  # 套用矩阵标准化的公式
+#     return datas
+
+# df=temp2(df)
+# print(df)
+
+# # 计算得分并归一化
+# def temp3(answer2):
+#     list_max = np.array(
+#         [np.max(answer2[0, :]), np.max(answer2[1, :]), np.max(answer2[2, :])])  # 获取每一列的最大值
+#     list_min = np.array(
+#         [np.min(answer2[0, :]), np.min(answer2[1, :]), np.min(answer2[2, :])])  # 获取每一列的最小值
+#     max_list = []  # 存放第i个评价对象与最大值的距离
+#     min_list = []  # 存放第i个评价对象与最小值的距离
+#     answer_list = []  # 存放评价对象的未归一化得分
+#     for k in range(0, np.size(answer2, axis=1)):  # 遍历每一列数据
+#         max_sum = 0
+#         min_sum = 0
+#         for q in range(0, 3):  # 有三个指标
+#             max_sum += np.power(answer2[q, k] - list_max[q], 2)  # 按每一列计算Di+
+#             min_sum += np.power(answer2[q, k] - list_min[q], 2)  # 按每一列计算Di-
+#         max_list.append(pow(max_sum, 0.5))
+#         min_list.append(pow(min_sum, 0.5))
+#         answer_list.append(min_list[k] / (min_list[k] + max_list[k]))  # 套用计算得分的公式 Si = (Di-) / ((Di+) +(Di-))
+#         max_sum = 0
+#         min_sum = 0
+#     answer = np.array(answer_list)  # 得分归一化
+#     return (answer / np.sum(answer))
+# print(temp3(df['周均供货量']))
+
+# df['周均供货量']=temp3(df['周均供货量'])
+# df['订单完成率']=temp3(df['订单完成率'])
+# df['总供货量']=temp3(df['总供货量'])
+
 def entropyWeight(data):
 	data = np.array(data)
 	# 归一化
@@ -31,6 +80,7 @@ def entropyWeight(data):
 	return (1 - E) / (1 - E).sum()
 
 entropyWeight(df)
+
 print(entropyWeight(df))
 
 def topsis(data, weight=None):
@@ -54,3 +104,5 @@ def topsis(data, weight=None):
 
 Result, Z, weight = topsis(df)
 Result.to_excel('result.xlsx')
+
+ 
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,1 +1,27 @@
-,sheet_name='sheet2'
+# 计算得分并归一化
+# def temp3(answer2):
+#     list_max = np.array(
+#         [np.max(answer2[0, :]), np.max(answer2[1, :]), np.max(answer2[2, :])])  # 获取每一列的最大值
+#     list_min = np.array(
+#         [np.min(answer2[0, :]), np.min(answer2[1, :]), np.min(answer2[2, :])])  # 获取每一列的最小值
+#     max_list = []  # 存放第i个评价对象与最大值的距离
+#     min_list = []  # 存放第i个评价对象与最小值的距离
+#     answer_list = []  # 存放评价对象的未归一化得分
+#     for k in range(0, np.size(answer2, axis=1)):  # 遍历每一列数据
+#         max_sum = 0
+#         min_sum = 0
+#         for q in range(0, 3):  # 有三个指标
+#             max_sum += np.power(answer2[q, k] - list_max[q], 2)  # 按每一列计算Di+
+#             min_sum += np.power(answer2[q, k] - list_min[q], 2)  # 按每一列计算Di-
+#         max_list.append(pow(max_sum, 0.5))
+#         min_list.append(pow(min_sum, 0.5))
+#         answer_list.append(min_list[k] / (min_list[k] + max_list[k]))  # 套用计算得分的公式 Si = (Di-) / ((Di+) +(Di-))
+#         max_sum = 0
+#         min_sum = 0
+#     answer = np.array(answer_list)  # 得分归一化
+#     return (answer / np.sum(answer))
+# print(temp3(df['周均供货量']))
+
+# df['周均供货量']=temp3(df['周均供货量'])
+# df['订单完成率']=temp3(df['订单完成率'])
+# df['总供货量']=temp3(df['总供货量'])
